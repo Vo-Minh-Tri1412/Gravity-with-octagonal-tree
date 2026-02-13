beb9b32e9bb87c15c1f5f72aacdf2045464c7033
Vo Minh Tri <2512724509@student.hcmus.edu.vn>
2026-02-13 22:56:30 +0700

    FEAT: Add timer to count time when run the simulation

--- src/Utils/Timer.hpp
+++ src/Utils/Timer.hpp
@@ -1,3 +1,29 @@
 #pragma once
+#include <chrono>
 
-// Empty
+namespace Utils
+{
+    class Timer
+    {
+    public:
+        void Start()
+        {
+            startTime = std::chrono::high_resolution_clock::now();
+        }
+
+        void Stop()
+        {
+            endTime = std::chrono::high_resolution_clock::now();
+        }
+
+        // Trả về thời gian đã trôi qua tính bằng mili-giây (ms)
+        float GetElapsedMS() const
+        {
+            return std::chrono::duration<float, std::milli>(endTime - startTime).count();
+        }
+
+    private:
+        std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
+        std::chrono::time_point<std::chrono::high_resolution_clock> endTime;
+    };
+}

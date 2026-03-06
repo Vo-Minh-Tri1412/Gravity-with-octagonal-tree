9329877db3da10fe7ed66c455774ae0c18e73387
Vo Minh Tri <2512724509@student.hcmus.edu.vn>
2026-03-06 16:15:55 +0700

    [FIX] : Fix some little error

--- src/Structure/Octree.cpp
+++ src/Structure/Octree.cpp
@@ -3,13 +3,10 @@
 Octree::Octree(const AABB &rootBounds, size_t maxCapacity)
     : initialBounds(rootBounds), nodePool(maxCapacity), root(nullptr) {}
 
-// hàm nay xây dựng lại cây mỗi khung hình, sẽ gọi liên tục trong vòng lặp
 void Octree::build(std::vector<Core::Particle> &particles)
 {
-    nodePool.reset(); // thay vì delete từng node mnhf chỉ cần reset con trỏ pool về 0=>Big O: O(1)
-
-    root = nodePool.allocate(initialBounds); // node gốc này bao trùm toàn bộ không gian .
-
+    nodePool.reset();
+    root = nodePool.allocate(initialBounds);
     for (auto &p : particles)
     {
         insert(root, &p);
@@ -17,17 +14,18 @@ void Octree::build(std::vector<Core::Particle> &particles)
     computeMassDistribution(root);
 }
 
-void Octree::insert(OctreeNode *node, Core::Particle *p)
+void Octree::insert(OctreeNode *node, Core::Particle *p, int depth)
 {
-    if (!node->boundary.contains(p->position))
-    {
+    if (depth > MAX_DEPTH)
         return;
-    } // check biên
-    // hạt bay ra ngoài vùng quản lý của node này thf bỏ qua
+
+    if (!node->boundary.contains(p->position))
+        return;
+
     if (node->isLeaf)
-    { // nếu đây là node lá
+    {
         if (node->particle == nullptr)
-        { // node đang trống:đặt hạt và kts thúc.
+        {
             node->particle = p;
         }
         else
@@ -35,27 +33,25 @@ void Octree::insert(OctreeNode *node, Core::Particle *p)
             Core::Particle *oldParticle = node->particle;
             node->particle = nullptr;
             node->isLeaf = false;
+
             for (int i = 0; i < 8; ++i)
-            { // th node có hạt thì mình sẽ chia thành 8 node con
                 node->children[i] = nodePool.allocate(node->boundary.getOctant(i));
-            }
-            insert(node, oldParticle); // chèn lại hạt cũ vào các node vừa tạo ỏ trên.
-            insert(node, p);
+
+            insert(node, oldParticle, depth + 1);
+            insert(node, p, depth + 1);
         }
     }
     else
     {
         glm::vec3 center = node->boundary.center;
-        glm::vec3 pos = p->position;
         int octantIndex = 0;
-        if (pos.x > center.x)
-            octantIndex |= 1; // x > tâm thf mnhf + 1
-        if (pos.y > center.y)
-            octantIndex |= 2; // y > tâm thf mnhf cộng 2
-        if (pos.z > center.z)
-            octantIndex |= 4; // z > tâm thì cộng 4
-
-        insert(node->children[octantIndex], p);
+        if (p->position.x > center.x)
+            octantIndex |= 1;
+        if (p->position.y > center.y)
+            octantIndex |= 2;
+        if (p->position.z > center.z)
+            octantIndex |= 4;
+        insert(node->children[octantIndex], p, depth + 1);
     }
 }
 
@@ -90,8 +86,6 @@ void Octree::computeMassDistribution(OctreeNode *node)
         }
 
         if (node->totalMass > 0.0f)
-        {
             node->centerOfMass /= node->totalMass;
-        }
     }
 }
